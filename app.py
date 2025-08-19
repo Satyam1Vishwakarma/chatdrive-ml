d25c09e5c4880a35d0ca3ccea409be924511ef92
Satyam Vishwakarma <satyam.k.vishwakarma@gmail.com>
2025-08-19 11:07:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,6 @@ def predict(data: UrlRequest):
     return {"prediction": int(prediction)}
 
 if __name__ == "__main__":
-    uvicorn.run(app)
-    #port = int(os.environ.get("PORT", 10000))
-    #uvicorn.run("main:app", host="0.0.0.0", port=port)
+    #uvicorn.run(app)
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
